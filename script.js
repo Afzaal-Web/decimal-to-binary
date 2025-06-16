4bb375ffb9ddc4a768a9fc283d7d918edaa53852
Afzaal Web <afzaalwebsolutions@gmail.com>
2025-06-16 16:49:05 +0500

    added some code in script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-const afzaal = document.getElementById('afzaal');
+const numberInput = document.getElementById('number-input');
+const convertBtn = document.getElementById('convert-btn');
+const result = document.getElementById('result');
