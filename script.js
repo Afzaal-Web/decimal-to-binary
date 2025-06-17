22da20bcc8cd36794b098450d3935c453aece9aa
Afzaal Web <afzaalwebsolutions@gmail.com>
2025-06-17 13:20:46 +0500

    Simple Recursive function Countdown Example

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const a = () => {
-  return "Javascript " + b();
-};
-
-const b = () => {
-  return "is " + c();
-};
-
-const c = () => {
-  return "Fun!";
-};
-
-console.log(a());
-
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+
+/* A recursive function is a function that calls itself over and over.
+But you have to be careful because you can easily create an infinite loop.
+That's where the base case comes in. The base case is when the function 
+stops calling itself, and it is a good idea to write it first. */
+
+const countdown = (number) => {
+  console.log(number);
+
+  if (number === 0) {
+    return;
+  } else {
+    countdown(number - 1);
+  }
+};
+
+countdown(20);
 
 const decimalToBinary = (input) => {
   let binary = "";
