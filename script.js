2bf42ac969acde5797a19815e96218f0dc5f3ae6
Afzaal Web <afzaalwebsolutions@gmail.com>
2025-06-17 14:21:30 +0500

    conversion of binary to decimal through recursion

--- script.js
+++ script.js
@@ -2,41 +2,15 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-/* A recursive function is a function that calls itself over and over.
-But you have to be careful because you can easily create an infinite loop.
-That's where the base case comes in. The base case is when the function 
-stops calling itself, and it is a good idea to write it first. */
-
-const countDownAndUp = (number) => {
-  console.log(number);
-
-  if (number === 0) {
-    console.log("Reached base case");
-    return;
-  } else {
-    countDownAndUp(number - 1 );
-    console.log(number);
-  }
-};
-
-countDownAndUp(3);
-
-
-
 const decimalToBinary = (input) => {
-  let binary = "";
-
-  if (input === 0) {
-    binary = "0";
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
 };
+
+
 
 const checkUserInput = () => {
   if (
@@ -48,7 +22,7 @@ const checkUserInput = () => {
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
 };
 
